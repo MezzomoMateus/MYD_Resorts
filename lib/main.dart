166bac63af0f70f8690789bb04bcf01f4fb27d3c
MezzomoMateus <mateusmezzomo10oito@gmail.com>
2024-10-01 19:43:22 -0300

    commit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io'; // Para manipular arquivos de imagem
 
 void main() {
   runApp(const MyApp());
@@ -11,103 +13,188 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ResortsScreen(),
+      home: InitialScreen(),
+      routes: {
+        '/resorts': (context) => ResortsScreen(),
+        '/cadastro': (context) => CadastroScreen(),
+        '/confirmacao': (context) => ConfirmacaoScreen(),
+      },
     );
   }
 }
 
-class ResortsScreen extends StatelessWidget {
-  const ResortsScreen({super.key});
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const HeaderSection(),
-          const Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: const [
-                  ResortCard(
-                    imageUrl: 'assets/ic_rs1.png',
-                    name: 'Resort Lagoas',
-                    location: 'Lisboa, PORT',
-                    price: r'$ 500,00 P/dia',
-                    rating: 5,
-                  ),
-                  ResortCard(
-                    imageUrl: 'assets/ic_rs2.png',
-                    name: 'Resort Olimpo',
-                    location: 'Florida, EUA',
-                    price: r'$ 835,00 P/dia',
-                    rating: 4,
-                  ),
-                  ResortCard(
-                    imageUrl: 'assets/ic_rs3.png',
-                    name: 'Hotel Caballero',
-                    location: 'Madrid, ESP',
-                    price: r'$ 780,00 P/dia',
-                    rating: 4,
-                  ),
-                ],
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'BEM VINDO AO',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
+            const SizedBox(height: 20),
+            Image.asset('assets/ic_logo.png', height: 150),
+            const SizedBox(height: 20),
+            ElevatedButton(
               onPressed: () {
-                // Ação do botão "CADASTRAR"
+                Navigator.pushNamed(context, '/resorts');
               },
+              style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                backgroundColor: Colors.white,
+              ),
               child: const Text(
-                'CADASTRAR NOVO RESORT',
+                'ENTRAR',
                 style: TextStyle(
+                  color: Colors.blue,
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white,
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-class HeaderSection extends StatelessWidget {
-  const HeaderSection({super.key});
+class ResortsScreen extends StatefulWidget {
+  const ResortsScreen({super.key});
+
+  @override
+  _ResortsScreenState createState() => _ResortsScreenState();
+}
+
+class _ResortsScreenState extends State<ResortsScreen> {
+  List<Map<String, dynamic>> resorts = [
+    {
+      'nome': 'Hotel Caballero',
+      'localizacao': 'Madrid, ESP',
+      'preco': r'R$ 780,00 P/dia',
+      'estrelas': 4,
+      'imagem': 'assets/ic_rs3.png',
+      'selected': false,
+    },
+  ];
+
+  void _adicionarResort(Map<String, dynamic> novoResort) {
+    setState(() {
+      novoResort['selected'] = false;
+      resorts.add(novoResort);
+    });
+  }
+
+  void _editarResort(int index, Map<String, dynamic> resortAtualizado) {
+    setState(() {
+      resortAtualizado['selected'] = false;
+      resorts[index] = resortAtualizado;
+    });
+  }
+
+  void _excluirResort(int index) {
+    setState(() {
+      resorts.removeAt(index);
+    });
+  }
+
+  void _editarResortCallback(int index) async {
+    final resort = resorts[index];
+    final resultado = await Navigator.pushNamed(
+      context,
+      '/cadastro',
+      arguments: {
+        'resort': resort,
+        'isEdit': true,
+        'index': index,
+      },
+    );
+
+    if (resultado != null) {
+      _editarResort(index, resultado as Map<String, dynamic>);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40.0),
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/ic_logo.png',
-            height: 164,
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+        title: const Text('Resorts'),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () {
+              final selectedResorts =
+                  resorts.where((resort) => resort['selected']).toList();
+              if (selectedResorts.length == 1) {
+                final index = resorts.indexOf(selectedResorts[0]);
+                _editarResortCallback(index);
+              } else if (selectedResorts.isEmpty) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content: Text('Nenhum resort selecionado para editar')),
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content: Text('Selecione apenas um resort para editar')),
+                );
+              }
+            },
           ),
-          const SizedBox(height: 10),
-          const Text(
-            'RESORTS',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              setState(() {
+                resorts.removeWhere((resort) => resort['selected']);
+              });
+            },
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: resorts.length,
+        itemBuilder: (context, index) {
+          final resort = resorts[index];
+          return ResortCard(
+            imageUrl: resort['imagem'],
+            name: resort['nome'],
+            location: resort['localizacao'],
+            price: resort['preco'],
+            rating: resort['estrelas'],
+            isSelected: resort['selected'],
+            onSelectedChanged: (bool? value) {
+              setState(() {
+                resort['selected'] = value!;
+              });
+            },
+            onDelete: () => _excluirResort(index),
+            onEdit: () => _editarResortCallback(index),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final novoResort = await Navigator.pushNamed(context, '/cadastro');
+          if (novoResort != null) {
+            _adicionarResort(novoResort as Map<String, dynamic>);
+          }
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
       ),
     );
   }
@@ -119,6 +206,10 @@ class ResortCard extends StatelessWidget {
   final String location;
   final String price;
   final int rating;
+  final bool isSelected;
+  final ValueChanged<bool?> onSelectedChanged;
+  final VoidCallback onDelete;
+  final VoidCallback onEdit;
 
   const ResortCard({
     super.key,
@@ -127,10 +218,38 @@ class ResortCard extends StatelessWidget {
     required this.location,
     required this.price,
     required this.rating,
+    required this.isSelected,
+    required this.onSelectedChanged,
+    required this.onDelete,
+    required this.onEdit,
   });
 
   @override
   Widget build(BuildContext context) {
+    Widget imageWidget;
+    if (imageUrl.startsWith('assets/')) {
+      imageWidget = Image.asset(
+        imageUrl,
+        width: 80,
+        height: 80,
+        fit: BoxFit.cover,
+      );
+    } else if (imageUrl.isNotEmpty) {
+      imageWidget = Image.file(
+        File(imageUrl),
+        width: 80,
+        height: 80,
+        fit: BoxFit.cover,
+      );
+    } else {
+      imageWidget = Container(
+        width: 80,
+        height: 80,
+        color: Colors.grey,
+        child: const Icon(Icons.image, color: Colors.white),
+      );
+    }
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
       child: Card(
@@ -142,12 +261,7 @@ class ResortCard extends StatelessWidget {
           contentPadding: const EdgeInsets.all(10),
           leading: ClipRRect(
             borderRadius: BorderRadius.circular(10),
-            child: Image.asset(
-              imageUrl,
-              width: 80,
-              height: 80,
-              fit: BoxFit.cover,
-            ),
+            child: imageWidget,
           ),
           title: Text(
             name,
@@ -182,7 +296,207 @@ class ResortCard extends StatelessWidget {
               ),
             ],
           ),
-          trailing: const Icon(Icons.check_box_outline_blank),
+          trailing: Checkbox(
+            value: isSelected,
+            onChanged: onSelectedChanged,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CadastroScreen extends StatefulWidget {
+  const CadastroScreen({super.key});
+
+  @override
+  _CadastroScreenState createState() => _CadastroScreenState();
+}
+
+class _CadastroScreenState extends State<CadastroScreen> {
+  final _nomeController = TextEditingController();
+  final _estrelasController = TextEditingController();
+  final _localizacaoController = TextEditingController();
+  final _precoController = TextEditingController();
+  File? _imagem;
+
+  bool isEdit = false;
+  int editIndex = -1;
+
+  @override
+  void didChangeDependencies() {
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    if (args != null) {
+      isEdit = args['isEdit'] ?? false;
+      if (isEdit) {
+        final resort = args['resort'] as Map<String, dynamic>;
+        editIndex = args['index'] ?? -1;
+        _nomeController.text = resort['nome'];
+        _estrelasController.text = resort['estrelas'].toString();
+        _localizacaoController.text = resort['localizacao'];
+        _precoController.text = resort['preco'].replaceAll('R\$ ', '');
+        if (resort['imagem'].toString().startsWith('assets/')) {
+          // Imagem é um asset
+          _imagem = null;
+        } else {
+          _imagem = resort['imagem'] != null && resort['imagem'].isNotEmpty
+              ? File(resort['imagem'])
+              : null;
+        }
+      }
+    }
+    super.didChangeDependencies();
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imagem = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _confirmarCadastro() {
+    final novoResort = {
+      'nome': _nomeController.text,
+      'estrelas': int.tryParse(_estrelasController.text) ?? 0,
+      'localizacao': _localizacaoController.text,
+      'preco': 'R\$ ${_precoController.text}',
+      'imagem': _imagem?.path ?? 'assets/ic_rs3.png',
+      'selected': false,
+    };
+
+    Navigator.pop(context, novoResort);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget imageWidget;
+    if (_imagem != null) {
+      imageWidget = Image.file(_imagem!, fit: BoxFit.cover);
+    } else {
+      imageWidget = const Center(child: Icon(Icons.add_a_photo));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isEdit ? 'Editar Resort' : 'Cadastrar Resort'),
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _nomeController,
+              decoration: const InputDecoration(labelText: 'Nome do Resort'),
+            ),
+            TextField(
+              controller: _estrelasController,
+              keyboardType: TextInputType.number,
+              decoration:
+                  const InputDecoration(labelText: 'Quantidade de Estrelas'),
+            ),
+            TextField(
+              controller: _localizacaoController,
+              decoration: const InputDecoration(labelText: 'Localização'),
+            ),
+            TextField(
+              controller: _precoController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Valor do Resort'),
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: _pickImage,
+              child: Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: imageWidget,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _confirmarCadastro,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(isEdit ? 'Salvar Alterações' : 'Cadastrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ConfirmacaoScreen extends StatelessWidget {
+  const ConfirmacaoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+    Widget imageWidget;
+    if (args['imagem'].toString().startsWith('assets/')) {
+      imageWidget = Image.asset(args['imagem'], height: 150);
+    } else if (args['imagem'] != null && args['imagem'].isNotEmpty) {
+      imageWidget = Image.file(File(args['imagem']), height: 150);
+    } else {
+      imageWidget = Container(
+        height: 150,
+        color: Colors.grey,
+        child: const Icon(Icons.image, color: Colors.white),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Confirmação do Resort'),
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            imageWidget,
+            const SizedBox(height: 20),
+            Text('Nome: ${args['nome']}'),
+            Text('Localização: ${args['localizacao']}'),
+            Text('Estrelas: ${args['estrelas']}'),
+            Text('Valor: ${args['preco']}'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.pop(context, {'edit': true});
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    Navigator.pop(context, {'delete': true});
+                  },
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, {'confirm': true});
+              },
+              child: const Text('Salvar'),
+            ),
+          ],
         ),
       ),
     );
